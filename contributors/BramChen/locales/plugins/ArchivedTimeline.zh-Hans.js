1a92e2d34a5ccfba5ceb7a7821a45763082cb3c7
Bram.Chen <Bram.Chen@50b3852a-b419-0410-861a-117b05584760>
2006-12-12 08:03:39 +0000

    Added property monthFormat

--- contributors/BramChen/locales/plugins/ArchivedTimeline.zh-Hans.js
+++ contributors/BramChen/locales/plugins/ArchivedTimeline.zh-Hans.js
@@ -7,6 +7,7 @@ if (typeof config.macros.archivedTimeline != "undefined"){
 	merge(config.macros.archivedTimeline, {
 		tooltips: "归档顺序: ",
 		orderBy: {modified: "修改日期", created: "创建日期"},
+		monthFormat: "YYYY年0MM月",
 		dateFormat: "YYYY年0MM月0DD日"
 	});
 }
