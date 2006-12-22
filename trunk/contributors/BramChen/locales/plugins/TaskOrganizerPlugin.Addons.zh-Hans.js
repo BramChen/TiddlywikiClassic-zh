4091854d3545dc16ecdf97de043d478e461dbdf0
Bram.Chen <Bram.Chen@50b3852a-b419-0410-861a-117b05584760>
2006-12-22 14:56:51 +0000

    Added zh* translations for ReminderPlugin,TaskOrganizerPlugin and TaskOrganizerPlugin.Addons Rename tags/builds-2.1.3-final to tags/2.1.3-final

---
+++ trunk/contributors/BramChen/locales/plugins/TaskOrganizerPlugin.Addons.zh-Hans.js
@@ -0,0 +1,13 @@
+/***
+!TaskOrganizerPlugin.Addons.zh-Hant
+***/
+//{{{
+if (typeof config.macros.taskOrganizer.options.nC != "undefined"){
+	merge(config.macros.taskOrganizer.options.nC, {
+		categoryTag: "类别",
+		taskTag: "专案",
+		newCategoryPrompt: "请输入类别名称：",
+		newCategoryTagPrompt: "请输入类别分类标签："
+	});
+}
+//}}}
