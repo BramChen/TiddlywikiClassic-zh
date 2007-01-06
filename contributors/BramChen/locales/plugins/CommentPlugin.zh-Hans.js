f99e7f74cda508b3d184aeea5308064f471a405a
Bram.Chen <Bram.Chen@50b3852a-b419-0410-861a-117b05584760>
2007-01-06 18:35:12 +0000

    Updated locale.zh* Fixed problem with some of shadowed tiddlers containing chinese chars would get the wrong encoding after saving changes on IE.

--- contributors/BramChen/locales/plugins/CommentPlugin.zh-Hans.js
+++ contributors/BramChen/locales/plugins/CommentPlugin.zh-Hans.js
@@ -5,8 +5,14 @@ Note: CommentPlugin modified by BramChen is required.
 //{{{
 if (typeof config.CommentPlugin != 'undefined'){
 	merge(config.CommentPlugin, {
- 		CPlingo:{CommentInTitle: " 回响 ", comments:"回响",add:"回应 »",edit:"编辑", tooltips:"发表关于此文的相关意见",	Title: '%0_CommentInTitle_%1'
- 		},
+		CPlingo:{
+			CommentInTitle: " 回响 ",
+			comments: "回响",
+			add: "回应 »",
+			edit: "编辑",
+			tooltips: "发表关于此文的相关意见",
+			Title: '%0_CommentInTitle_%1'
+		}
 	});
 	CPlingo = config.CommentPlugin.CPlingo;
 }
