b9a98f0b5ca5ddb21c6d10be81c9dfd02842ddda
Bram.Chen <Bram.Chen@50b3852a-b419-0410-861a-117b05584760>
2007-03-01 08:29:53 +0000

    Added NewHerePlugin, TagglyTaggingPlugin zh locales

---
+++ trunk/contributors/BramChen/locales/plugins/TagglyTaggingPlugin.zh-Hans.js
@@ -0,0 +1,37 @@
+/***
+!TagglyTaggingPlugin.zh-Hans.js
+***/
+//{{{
+merge(config.taggly,{
+	lingo: {
+		labels: {
+			asc:		"\u2191", // 向下箭号
+			desc:		"\u2193", // 向上箭号
+			title:		"文章标题",
+			modified:	"更新日期",
+			created:	"创建日期",
+			show:		"+",
+			hide:		"-",
+			normal:		"一般",
+			group:		"分群",
+			commas:		"逗点",
+			sitemap:	"导览",
+			numCols:	"栏数\u00b1", // 加减号
+			label:		"标签 '%0':"
+		},
+
+		tooltips: {
+			title:		"依文章标题排序",
+			modified:	"依文章更新日期排序",
+			created:	"依文章创建日期排序",
+			show:		"显示 tagging 列表",
+			hide:		"隐藏 tagging 列表",
+			normal:		"不分群显示",
+			group:		"依标签分群显示",
+			sitemap:	"改以站台导览风格显示",
+			commas:		"改以逗点区隔列表",
+			numCols:	"更改显示之栏位数"
+		}
+	}
+});
+//}}}
---
+++ trunk/contributors/BramChen/locales/plugins/TagglyTaggingPlugin.zh-Hant.js
@@ -0,0 +1,37 @@
+/***
+!TagglyTaggingPlugin.zh-Hant.js
+***/
+//{{{
+merge(config.taggly,{
+	lingo: {
+		labels: {
+			asc:		"\u2191", // 向下箭號
+			desc:		"\u2193", // 向上箭號
+			title:		"文章標題",
+			modified:	"更新日期",
+			created:	"建立日期",
+			show:		"+",
+			hide:		"-",
+			normal:		"一般",
+			group:		"分群",
+			commas:		"逗點",
+			sitemap:	"導覽",
+			numCols:	"欄數\u00b1", // 加減號
+			label:		"標籤 '%0':"
+		},
+
+		tooltips: {
+			title:		"依文章標題排序",
+			modified:	"依文章更新日期排序",
+			created:	"依文章建立日期排序",
+			show:		"顯示 tagging 列表",
+			hide:		"隱藏 tagging 列表",
+			normal:		"不分群顯示",
+			group:		"依標籤分群顯示",
+			sitemap:	"改以站台導覽風格顯示",
+			commas:		"改以逗點區隔列表",
+			numCols:	"更改顯示之欄位數"
+		}
+	}
+});
+//}}}
