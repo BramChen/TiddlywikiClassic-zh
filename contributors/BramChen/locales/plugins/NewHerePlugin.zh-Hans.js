b2e04f860200ae9869584b64fcc62642d0a75257
Bram.Chen <Bram.Chen@50b3852a-b419-0410-861a-117b05584760>
2007-03-01 08:29:53 +0000

    Added NewHerePlugin, TagglyTaggingPlugin zh locales

---
+++ contributors/BramChen/locales/plugins/NewHerePlugin.zh-Hans.js
@@ -0,0 +1,14 @@
+/***
+!NewHerePlugin.js.zh-Hans
+***/
+//{{{
+merge(config.commands,{
+	newHere: {
+		text: "创建文章",
+		tooltip: "创建以本文章标题为标签的文章"},
+	newJournalHere: {
+		text: "创建日志",
+		dataFormat: "YYYY年0MM月0DD日",
+		tooltip: "创建以本文章标题为标签的日志"}
+});
+//}}}
---
+++ contributors/BramChen/locales/plugins/NewHerePlugin.zh-Hant.js
@@ -0,0 +1,14 @@
+/***
+!NewHerePlugin.js.zh-Hant
+***/
+//{{{
+merge(config.commands,{
+	newHere: {
+		text: "新增文章",
+		tooltip: "新增以本文章標題為標籤的文章"},
+	newJournalHere: {
+		text: "新增日誌",
+		dataFormat: "YYYY年0MM月0DD日",
+		tooltip: "新增以本文章標題為標籤的日誌"}
+});
+//}}}
