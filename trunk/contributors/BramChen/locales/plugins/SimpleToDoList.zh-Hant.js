9d2066150dce82201493e82aa94c05ff829992ac
Bram.Chen <Bram.Chen@50b3852a-b419-0410-861a-117b05584760>
2007-04-16 03:53:39 +0000

    Added zh locales for SimpleToDoList

---
+++ trunk/contributors/BramChen/locales/plugins/SimpleToDoList.zh-Hant.js
@@ -0,0 +1,22 @@
+/***
+|''Name:''|SimpleToDoList.zh-Hant|
+|''Source:''|[[TiddlyWiki-zh|http://tiddlywiki-zh.googlecode.com/svn/trunk/contributors/BramChen/locales/plugins/]]|
+|''Requires:''|[[SimpleToDoList]]|
+***/
+//{{{
+if (typeof config.macros.stlManager != "undefined"){
+	merge(config.macros.stlManager,{
+		wizardTitle: "",
+		step1Title: "",
+		step1Html: "<input type='hidden' name='markList'></input>", // DO NOT TRANSLATE
+		listViewTemplate: {
+			columns: [
+				{name: 'Completed', field: 'completed', title: "完成？", tag: 'completed', type: 'TagCheckbox'},
+				{name: 'Title', field: 'title', tiddlerLink: 'title', title: "項目", type: 'TiddlerLink'},
+				{name: 'Text', field: 'text', title: '說明', type: 'WikiText'}
+				],
+			rowClasses: [
+				]}
+	});
+}
+//}}}
