b41f200b63fadbc4e593fb483e382569278555ee
Bram.Chen <Bram.Chen@50b3852a-b419-0410-861a-117b05584760>
2007-04-30 01:51:50 +0000

    Up to date with official changeset #2132

--- trunk/contributors/BramChen/locales/plugins/NewHerePlugin.zh-Hans.js
+++ trunk/contributors/BramChen/locales/plugins/NewHerePlugin.zh-Hans.js
@@ -5,13 +5,13 @@
 ***/
 
 //{{{
-merge(config.commands,{
+merge(config.macros,{
 	newHere: {
-		text: "创建文章",
-		tooltip: "创建以本文章标题为标签的文章"},
+		label: "创建文章",
+		prompt: "创建以本文章标题为标签的文章"},
 	newJournalHere: {
-		text: "创建日志",
-		dataFormat: "YYYY年0MM月0DD日",
-		tooltip: "创建以本文章标题为标签的日志"}
+		label: "创建日志",
+		dateFormat: "YYYY年0MM月0DD日",
+		prompt: "创建以本文章标题为标签的日志"}
 });
 //}}}
