1180012d1ced95e720ad2c20d45522a683f03311
Bram.Chen <Bram.Chen@50b3852a-b419-0410-861a-117b05584760>
2007-10-09 06:12:35 +0000

    Update locale files of UploadPlugin

--- trunk/contributors/BramChen/locales/plugins/UploadPlugin.zh-Hans.js
+++ trunk/contributors/BramChen/locales/plugins/UploadPlugin.zh-Hans.js
@@ -1,56 +1,74 @@
 /***
-|''Name:''|UploadPlugin.zh-Hans|
+|''Name:''|UploadPlugin.zh-Hant|
 |''Source:''|[[TiddlyWiki-zh|http://tiddlywiki-zh.googlecode.com/svn/trunk/contributors/BramChen/locales/plugins/]]|
 |''Require:''|[[UploadPlugin|http://tiddlywiki.bidix.info/#UploadPlugin]]|
 ***/
 //{{{
 if (typeof config.macros.upload != "undefined"){
 	merge(config.macros.upload.messages, {
-		aboutToUpload: "关于上传 TiddlyWiki 至 %0",
-		backupFileStored: "前一版的文件备份于 %0",
-		crossDomain: "发生跨网域问题：不允许存取其他网站",
-		errorDownloading: "下载时发生错误",
-		errorUploadingContent: "传时发生错误",
-		fileLocked: "文件被锁住：目前不允许上传",
-		fileNotFound: "无此上传文件",
-		fileNotUploaded: "文件 %0 未完成上传",
-		mainFileUploaded: "主文件 TiddlyWiki 上传至 %0",
-		passwordEmpty: "请先输入密码后再上传",
-		urlParamMissing: "未指定 url 参数",
-		rssFileNotUploaded: "Rss 文件 %0 未上传",
-		rssFileUploaded: "Rss 文件传至 %0"
+		noStoreUrl: "未指定 store URL 参数或设置选项",
+		usernameOrPasswordMissing: "帐号或密码不正确"
 	});
-	
+
 	merge(config.macros.upload.label, {
-		promptOption: " 依据 UploadOptions 的设置，保存并上传此 TiddlyWiki",
+		promptOption: " 依据 UploadOptions 的设定，保存并上传此 TiddlyWiki",
 		promptParamMacro: "储存并上传此 TiddlyWiki 文件于 %0",
 		saveLabel: "远端保存", 
 		saveToDisk: "本机保存",
 		uploadLabel: "上传"	
 	});
+
+	merge(config.macros.uploadOptions, {
+		wizardTitle: "设置上传之选项",
+		step1Title: "这些设置将保存于浏览器的 cookies",
+		step1Html: "<input type='hidden' name='markList'></input><br>",
+		cancelButton: "取消",
+		cancelButtonPrompt: "取消设定",
+		listViewTemplate: {
+			columns: [
+				{name: 'Description', field: 'description', title: "说明", type: 'WikiText'},
+				{name: 'Option', field: 'option', title: "选项", type: 'String'},
+				{name: 'Name', field: 'name', title: "名称", type: 'String'}
+				],
+			rowClasses: [
+				{className: 'lowlight', field: 'lowlight'} 
+				]}
+		});
+
+	bidix.upload.messages = {
+		//from saving
+		invalidFileError: "原始文件 '%0' 非 TiddlyWiki 文件",
+		backupSaved: "备份文件已上传至主机",
+		backupFailed: "备份文件上传至主机失败",
+		rssSaved: "已上传Rss 文件",
+		rssFailed: "Rss 文件上传失败",
+		emptySaved: "已上传空白范本",
+		emptyFailed: "上传空白范本失败",
+		mainSaved: "已上传 TiddlyWiki",
+		mainFailed: "上传 TiddlyWiki 失败，所作变更未完成保存",
+		//specific upload
+		loadOriginalHttpPostError: "无法载入原始文件",
+		aboutToSaveOnHttpPost: '关于上传 TiddlyWiki 至 %0 ...',
+		storePhpNotFound: "无服务端 store script '%0'。"
+	};
+
+	merge(config.optionsDesc,{
+		txtUploadStoreUrl: "上传服务程式之 Url (预设：store.php)",
+		txtUploadFilename: "上传文件 (预设：index.html)",
+		txtUploadDir: "保存文件之相对路径 (预设：. (downloadService directory))",
+		txtUploadBackupDir: "保存备份文件之相对路径，若为空白则不作备份. (预设：''(空白))",
+		txtUploadUserName: "帐号",
+		pasUploadPassword: "密码",
+		chkUploadLog: "是否纪录于 UploadLog (预设：true)",
+		txtUploadLogMaxLine: "UploadLog 纪录上限(预设：10)"
+	});
 	
+	merge(config.tasks,{
+		uploadOptions: {text: "上传", tooltip: "修改上传设置, content: '<<uploadOptions>>'}
+	});
+
 	config.macros.saveChanges.label = (document.location.toString().substr(0,4) == "http")?"远端保存":"本机保存";
-	config.macros.option.passwordCheckboxLabel = "保存此密码于本机";
-	config.shadowTiddlers['OptionsPanel'] += '\n[[上传选项|UploadOptions]]';
-	config.shadowTiddlers['UploadOptions'] = "\
-!UploadPlugin 选项設置\n\
-帐号：<<option txtUploadUserName>>\n\
-密码：<<option pasUploadPassword>>\n\n\
-上传的服务程式的网址^^(1)^^: <<option txtUploadStoreUrl 50>>\n\
-保存文件的相对资料夹^^(2)^^: <<option txtUploadDir 50>>\n\
-上传文件的档名^^(3)^^: <<option txtUploadFilename 40>>\n\
-保存备份文件于伺服主机上的资料夹^^(4)^^: <<option txtUploadBackupDir>>\n\n\
-^^(1)^^可于[[上传选项|UploadOptions]] 或使用宏的参数设置\n\
-^^(2)^^若为空白，则与服务程式同资料夹\n\
-^^(3)^^若为空白，则与使用原文件档名\n\
-^^(4)^^若为空白，于伺服主机之相同档名的文件将被覆写取代\n\n\
-依上述设定 <<upload>>\n\n\
-!Upload 宏的语法范例\n\
-{{{\n\
-<<upload [storeUrl [toFilename [backupDir [uploadDir [username]]]]]>>\n\
-}}}\n\
-	选用之参数可以取代[[上传选项|UploadOptions]] 的設置\n\
-	";
+	config.macros.option.passwordCheckboxLabel = "存放此密码于本机";
 
 }
 //}}}
--- trunk/contributors/BramChen/locales/plugins/UploadPlugin.zh-Hant.js
+++ trunk/contributors/BramChen/locales/plugins/UploadPlugin.zh-Hant.js
@@ -6,21 +6,10 @@
 //{{{
 if (typeof config.macros.upload != "undefined"){
 	merge(config.macros.upload.messages, {
-		aboutToUpload: "關於上傳 TiddlyWiki 至 %0",
-		backupFileStored: "前一版的檔案備份於 %0",
-		crossDomain: "發生跨網域問題：不允許存取其他網站",
-		errorDownloading: "下載時發生錯誤",
-		errorUploadingContent: "傳時發生錯誤",
-		fileLocked: "檔案被鎖住：目前不允許上傳",
-		fileNotFound: "無此上傳檔案",
-		fileNotUploaded: "檔案 %0 未完成上傳",
-		mainFileUploaded: "主文件 TiddlyWiki 上傳至 %0",
-		passwordEmpty: "請先輸入密碼後再上傳",
-		urlParamMissing: "未指定 url 參數",
-		rssFileNotUploaded: "Rss 檔案 %0 未上傳",
-		rssFileUploaded: "Rss 檔上傳至 %0"
+		noStoreUrl: "未指定 store URL 參數或設定選項",
+		usernameOrPasswordMissing: "帳號或密碼不正確"
 	});
-	
+
 	merge(config.macros.upload.label, {
 		promptOption: " 依據 UploadOptions 的設定，儲存並上傳此 TiddlyWiki",
 		promptParamMacro: "儲存並上傳此 TiddlyWiki 文件於 %0",
@@ -28,29 +17,58 @@ if (typeof config.macros.upload != "undefined"){
 		saveToDisk: "本機存檔",
 		uploadLabel: "上傳"	
 	});
+
+	merge(config.macros.uploadOptions, {
+		wizardTitle: "設定上傳之選項",
+		step1Title: "這些設定將儲存於瀏覽器的 cookies",
+		step1Html: "<input type='hidden' name='markList'></input><br>",
+		cancelButton: "取消",
+		cancelButtonPrompt: "取消設定",
+		listViewTemplate: {
+			columns: [
+				{name: 'Description', field: 'description', title: "說明", type: 'WikiText'},
+				{name: 'Option', field: 'option', title: "選項", type: 'String'},
+				{name: 'Name', field: 'name', title: "名稱", type: 'String'}
+				],
+			rowClasses: [
+				{className: 'lowlight', field: 'lowlight'} 
+				]}
+		});
+
+	bidix.upload.messages = {
+		//from saving
+		invalidFileError: "原始文件 '%0' 非 TiddlyWiki 文件",
+		backupSaved: "備份檔已上傳至主機",
+		backupFailed: "備份檔上傳至主機失敗",
+		rssSaved: "已上傳Rss 檔案",
+		rssFailed: "Rss 檔案上傳失敗",
+		emptySaved: "已上傳空白範本",
+		emptyFailed: "上傳空白範本失敗",
+		mainSaved: "已上傳 TiddlyWiki",
+		mainFailed: "上傳 TiddlyWiki 失敗，所作變更未完成儲存",
+		//specific upload
+		loadOriginalHttpPostError: "無法載入原始檔案",
+		aboutToSaveOnHttpPost: '關於上傳 TiddlyWiki 至 %0 ...',
+		storePhpNotFound: "無伺服端 store script '%0'。"
+	};
+
+	merge(config.optionsDesc,{
+		txtUploadStoreUrl: "上傳伺服程式之 Url (預設：store.php)",
+		txtUploadFilename: "上傳檔案 (預設：index.html)",
+		txtUploadDir: "儲存檔案之相對路徑 (預設：. (downloadService directory))",
+		txtUploadBackupDir: "儲存備份檔案之相對路徑，若為空白則不作備份. (預設：''(空白))",
+		txtUploadUserName: "帳號",
+		pasUploadPassword: "密碼",
+		chkUploadLog: "是否紀錄於 UploadLog (預設：true)",
+		txtUploadLogMaxLine: "UploadLog 紀錄上限(預設：10)"
+	});
 	
+	merge(config.tasks,{
+		uploadOptions: {text: "上傳", tooltip: "修改上傳設定", content: '<<uploadOptions>>'}
+	});
+
 	config.macros.saveChanges.label = (document.location.toString().substr(0,4) == "http")?"遠端存檔":"本機存檔";
 	config.macros.option.passwordCheckboxLabel = "存放此密碼於本機";
-	config.shadowTiddlers['OptionsPanel'] += '\n[[上傳選項|UploadOptions]]';
-	config.shadowTiddlers['UploadOptions'] = "\
-!UploadPlugin 選項設定\n\
-帳號：<<option txtUploadUserName>>\n\
-密碼：<<option pasUploadPassword>>\n\n\
-上傳的服務程式的網址^^(1)^^: <<option txtUploadStoreUrl 50>>\n\
-存放檔案的相對資料夾^^(2)^^: <<option txtUploadDir 50>>\n\
-上傳檔案的檔名^^(3)^^: <<option txtUploadFilename 40>>\n\
-存放備份檔於伺服主機上的資料夾^^(4)^^: <<option txtUploadBackupDir>>\n\n\
-^^(1)^^可於[[上傳選項|UploadOptions]] 或使用巨集參數設定\n\
-^^(2)^^若為空白，則與服務程式同資料夾\n\
-^^(3)^^若為空白，則與使用原文件檔名\n\
-^^(4)^^若為空白，於伺服主機之相同檔名的檔案將被覆寫取代\n\n\
-依上述設定 <<upload>>\n\n\
-!Upload 巨集語法範例\n\
-{{{\n\
-<<upload [storeUrl [toFilename [backupDir [uploadDir [username]]]]]>>\n\
-}}}\n\
-	選用之參數可以取代[[上傳選項|UploadOptions]] 的設定\n\
-	";
 
 }
 //}}}
